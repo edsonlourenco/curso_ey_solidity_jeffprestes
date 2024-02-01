5d2207a7861ad200ec3c8563cf15fea3d59d8f79
Edson Lourenço <edson.lourenco.dev@gmail.com>
2024-02-01 20:00:16 -0300

    implementação e teste do Aluguel

--- contracts/nuclea-aula.sol
+++ contracts/nuclea-aula.sol
@@ -2,15 +2,32 @@
 SPDX-License-Identifier: CC-BY-4.0
 (c) Desenvolvido por Jeff Prestes
 This work is licensed under a Creative Commons Attribution 4.0 International License.
+
+Endereço do contrato na rede Sepolia (dados de todo o processo):
+
+- Transaction Hash: 0xbc7c66d4d83016aa6c3c3818c8c67cfe9966196785b16cb79735f30223947f56
+- From: 0xAB63b295DC6567C1E29AC2A8559D85fFa0b70914
+- To: 0x7117c624b223C7C1865E29C89c3a9Da1Ca84831d
+
+No PNG aluguel_sol_deploy.png estão os dados printados da execução.
+
 */
-pragma solidity >=0.8.2 <0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
 
 contract Aluguel {
+    
+    struct DadosPagamento {
+        uint quandoFoiPago;
+        uint valorPago;
+    }
+    
     string public locatario;
     string public locador;
     uint256 private valor;
     uint256 constant public numeroMaximoLegalDeAlugueisParaMulta = 3;
-    bool[] public statusPagamento;
+    DadosPagamento[] public statusPagamento;
+
+
     /*
     0 - 01/2020 = true
     1 - 02/2020 = true
@@ -43,11 +60,11 @@ contract Aluguel {
         return valorMulta;
     } 
         
-    function reajustaAluguel(uint256 percentualReajuste) public 
-    {
+    function reajustaAluguel(uint256 percentualReajuste) public {
+        require(msg.sender == owner, "somente o dono do imovel pode reajustar o aluguel");
         if (percentualReajuste > 20) {
             percentualReajuste = 20;
-     }
+        }
         uint256 valorDoAcrescimo = 0;
         valorDoAcrescimo = ((valor*percentualReajuste)/100);
         valor = valor + valorDoAcrescimo;
@@ -65,10 +82,11 @@ contract Aluguel {
     }
     
     
-    function receberPagamento() public payable {
+    function receberPagamento() public payable {        
         require(msg.value>=valor, "Valor insuficiente");
         contaLocador.transfer(msg.value);
-        statusPagamento.push(true);
+        DadosPagamento memory dPgto = DadosPagamento(block.timestamp, msg.value);
+        statusPagamento.push(dPgto);
     }
     
     //msg.value = valor em wei enviado ao contrato
@@ -83,5 +101,13 @@ contract Aluguel {
     
     function quantosPagamentosJaForamFeitos() public view returns (uint256) {
         return statusPagamento.length;
+    }
+
+    function historicoDePagto() public view returns (uint valorTotalRecebido, uint numAluguelRecebido) {
+        for (uint256 i; i < statusPagamento.length; i++) {
+            valorTotalRecebido += statusPagamento[i].valorPago;
+            numAluguelRecebido++;
+        }
+        return (valorTotalRecebido, numAluguelRecebido);
     }
 }
